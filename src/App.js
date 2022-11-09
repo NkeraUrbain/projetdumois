bc21220a4041e85688906c54cf6c17f06cf6a026
Nkera Urbain <urbainmuhamiriza1990@gmail.com>
2022-11-09 11:11:53 +0100

    lier le repo local au repo distant

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Connexion from './Connexion';
+import EnvoiMessage from './EnvoiMessage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <Connexion/>
+          <EnvoiMessage/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
